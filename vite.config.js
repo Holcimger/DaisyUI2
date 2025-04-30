ee15138522af8d7d6a7be3513e54481a92b86aa1
Holcimger <maximilian.springer@holcim.com>
2025-04-30 09:21:52 -0300

    changed viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/your-repository-name/", // Change this to match your repo name!
+  base: "/DaisyUI2/", // Change this to match your repo name!
   build: {
     outDir: "dist",
     assetsDir: "",
